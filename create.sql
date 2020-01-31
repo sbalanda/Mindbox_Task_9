ab5d059dac9cbca5c7d82e065d353a1cb8ba9365
Sergey Balanda <49357627+sbalanda@users.noreply.github.com>
2020-01-31 11:07:56 +0300

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,5 +1,5 @@
 create table Product_Category
 (
     ProductId int foreign key(Products.ProductId),
-    CategoryId int foreign(Categories.CategoryId)
+    CategoryId int foreign key(Categories.CategoryId)
 );
